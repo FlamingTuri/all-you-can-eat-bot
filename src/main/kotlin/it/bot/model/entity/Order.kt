e9714b789a05573bbd85c98407b0bede35497a7e
gventurini <giacomo.venturini3@gmail.com>
2022-04-19 18:39:40 +0200

    add sequence, generator and make enum field serialized as string

--- src/main/kotlin/it/bot/model/entity/Order.kt
+++ src/main/kotlin/it/bot/model/entity/Order.kt
@@ -3,20 +3,33 @@ package it.bot.model.entity
 import it.bot.model.enum.OrderStatus
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
 
+private const val TABLE = "order"
+private const val SEQUENCE = TABLE + "_sequence"
+private const val GENERATOR = TABLE + "_generator"
+
 @Entity
-@Table(name = "order")
+@Table(name = TABLE)
+@SequenceGenerator(name = GENERATOR, sequenceName = SEQUENCE, allocationSize = 1)
 class Order : AbstractEntity() {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = GENERATOR)
     @Column(name = "order_id", nullable = false)
-    var orderId: Long? = null
+    var id: Long? = null
 
     @Column(nullable = false)
     var name: String? = null
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     var status: OrderStatus = OrderStatus.Open
 
