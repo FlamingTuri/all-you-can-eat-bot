1fc9ba2693a5921e0eec36f1846bf752938eb269
gventurini <giacomo.venturini3@gmail.com>
2022-04-19 18:54:36 +0200

    add error message when a non supported command is sent

--- src/main/kotlin/it/bot/AllYouCanEatBot.kt
+++ src/main/kotlin/it/bot/AllYouCanEatBot.kt
@@ -4,24 +4,26 @@ import io.quarkus.logging.Log
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
 class AllYouCanEatBot(private val botToken: String) : TelegramLongPollingBot() {
 
     override fun onUpdateReceived(update: Update) {
         println("update received")
         if (update.hasMessage() && update.message.hasText()) {
-            val messageText = update.message.text
-            val chatId = update.message.chatId.toString()
-            val message = SendMessage()
-            message.chatId = chatId
-            message.text = messageText
-            try {
-                execute(message)
-            } catch (e: TelegramApiException) {
-                Log.error(e)
+            when (update.message.text) {
+                "/createOrder" -> {}//TODO
+                else -> sendErrorMessage(update)
             }
         }
+    }
+
+    private fun sendErrorMessage(update: Update) {
+        val errorMessage = "no command support for '${update.message.text}'"
+        Log.error(errorMessage)
+        val message = SendMessage()
+        message.chatId = update.message.chatId.toString()
+        message.text = errorMessage
+        execute(message)
     }
 
     override fun getBotUsername(): String {
