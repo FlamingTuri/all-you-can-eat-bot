19c78cac6dea60041d092ef07aecc969810c4581
gventurini <giacomo.venturini3@gmail.com>
2022-04-24 11:41:31 +0200

    add leave order service

---
+++ src/main/kotlin/it/bot/service/impl/LeaveOrderService.kt
@@ -0,0 +1,34 @@
+package it.bot.service.impl
+
+import it.bot.repository.OrderRepository
+import it.bot.service.interfaces.CommandParserService
+import it.bot.util.MessageUtils
+import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
+import javax.transaction.Transactional
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+
+@ApplicationScoped
+class LeaveOrderService(@Inject private val orderRepository: OrderRepository) : CommandParserService() {
+
+    override val command = "/leaveOrder"
+
+    override val commandPattern = "(\\s*)(\\w+)(\\s*)"
+
+    @Transactional
+    override fun parseUpdate(update: Update): SendMessage? {
+        return super.parseUpdate(update)
+    }
+
+    override fun executeOperation(update: Update, matchResult: MatchResult): SendMessage? {
+        val (_, orderName, _) = matchResult.destructured
+
+        val chatId = MessageUtils.getChatId(update)
+        val order = orderRepository.findOpenOrderWithNameForChat(chatId, orderName)
+
+        // TODO
+
+        return null
+    }
+}
