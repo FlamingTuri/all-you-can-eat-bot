873a28c6f7dc1300a2f3a001d8bdb1dfc41ff656
gventurini <giacomo.venturini3@gmail.com>
2022-04-25 22:34:06 +0200

    prevent user to join a closed order

--- src/main/kotlin/it/bot/service/impl/JoinOrderService.kt
+++ src/main/kotlin/it/bot/service/impl/JoinOrderService.kt
@@ -1,11 +1,12 @@
 package it.bot.service.impl
 
-import io.quarkus.logging.Log
 import it.bot.model.entity.UserEntity
+import it.bot.model.enum.OrderStatus
 import it.bot.repository.OrderRepository
 import it.bot.repository.UserRepository
 import it.bot.service.interfaces.CommandParserService
 import it.bot.util.MessageUtils
+import it.bot.util.OrderUtils
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 import javax.transaction.Transactional
@@ -39,21 +40,19 @@ class JoinOrderService(
         val chatId = MessageUtils.getChatId(update)
         val order = orderRepository.findOpenOrderWithNameForChat(chatId, orderName)
 
-        if (order == null) {
-            Log.error("order $orderName does not exists in chat $chatId")
-            return MessageUtils.createMessage(
-                update,
-                "Error: you can not join $orderName order, since it does not exists in the current chat"
-            )
+        return when {
+            order == null -> OrderUtils.getOrderNotFoundMessage(update, orderName)
+            order?.status == OrderStatus.Close -> OrderUtils.getOrderMustBeInOpenStateMessage(update, orderName)
+            else -> {
+                val userEntity = UserEntity().apply {
+                    telegramUserId = MessageUtils.getTelegramUserId(update)
+                    this.order = order
+                }
+
+                userRepository.persist(userEntity)
+
+                MessageUtils.createMessage(update, "Successfully joined order $orderName")
+            }
         }
-
-        val userEntity = UserEntity().apply {
-            telegramUserId = MessageUtils.getTelegramUserId(update)
-            this.order = order
-        }
-
-        userRepository.persist(userEntity)
-
-        return MessageUtils.createMessage(update, "Successfully joined order $orderName")
     }
 }
