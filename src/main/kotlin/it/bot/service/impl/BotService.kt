710acf5bce896a9075a1e4bd27ecbc480e9a80a2
gventurini <giacomo.venturini3@gmail.com>
2022-04-30 23:34:49 +0200

    add missing private keyword in constructor members

--- src/main/kotlin/it/bot/service/impl/BotService.kt
+++ src/main/kotlin/it/bot/service/impl/BotService.kt
@@ -17,16 +17,16 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 class BotService(
     @ConfigProperty(name = "bot.username") private val botUsername: String,
     @ConfigProperty(name = "bot.token") private val botToken: String,
-    @Inject val createOrderService: CreateOrderService,
-    @Inject val joinOrderService: JoinOrderService,
-    @Inject val leaveOrderService: LeaveOrderService,
-    @Inject val addDishService: AddDishService,
-    @Inject val closeOrderService: CloseOrderService,
-    @Inject val openOrderService: OpenOrderService,
-    @Inject val blameDishService: BlameDishService,
-    @Inject val showOrderService: ShowOrderService,
-    @Inject val nameDishService: NameDishService,
-    @Inject val removeDishService: RemoveDishService
+    @Inject private val createOrderService: CreateOrderService,
+    @Inject private val joinOrderService: JoinOrderService,
+    @Inject private val leaveOrderService: LeaveOrderService,
+    @Inject private val addDishService: AddDishService,
+    @Inject private val closeOrderService: CloseOrderService,
+    @Inject private val openOrderService: OpenOrderService,
+    @Inject private val blameDishService: BlameDishService,
+    @Inject private val showOrderService: ShowOrderService,
+    @Inject private val nameDishService: NameDishService,
+    @Inject private val removeDishService: RemoveDishService
 ) {
 
     private val botsApi = TelegramBotsApi(DefaultBotSession::class.java)
