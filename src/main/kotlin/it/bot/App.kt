a204f53256de83998f6cab0d468d34a9a2054999
gventurini <giacomo.venturini3@gmail.com>
2022-05-17 23:01:57 +0200

    add logging of app active profile

--- src/main/kotlin/it/bot/App.kt
+++ src/main/kotlin/it/bot/App.kt
@@ -4,6 +4,7 @@ import io.quarkus.logging.Log
 import io.quarkus.runtime.Quarkus
 import io.quarkus.runtime.QuarkusApplication
 import io.quarkus.runtime.annotations.QuarkusMain
+import io.quarkus.runtime.configuration.ProfileManager
 
 /**
  * Overriding default quarkus main allows to debug the application without having to install a quarkus plugin
@@ -19,7 +20,7 @@ object App {
     class MyApp : QuarkusApplication {
         @Throws(Exception::class)
         override fun run(vararg args: String): Int {
-            Log.info("App successfully initialized")
+            Log.info("App successfully initialized, active profile ${ProfileManager.getActiveProfile()}")
             Quarkus.waitForExit()
             return 0
         }
