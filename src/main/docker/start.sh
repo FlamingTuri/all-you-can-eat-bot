a84b34107425ba85da7a11ac16f9de59deb80c83
gventurini <giacomo.venturini3@gmail.com>
2022-05-22 21:57:05 +0200

    decrease keep alive job run frequence

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -42,6 +42,7 @@ java \
   -DBOT_REPO_URL="${BOT_REPO_URL}" \
   -DBOT_DONATE_URL="${BOT_DONATE_URL}" \
   -DCRON_ENABLED=true \
+  -DKEEP_ALIVE_JOB_CRON_EXPR="0 */45 * * * ?" \
   -DCLEANUP_JOB_CRON_EXPR="0 0 */12 * * ?" \
   -Dall-you-can-eat-bot-rest-client/mp-rest/url="${BOT_REST_URL}" \
   -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
