34a63004d706883d39185a6ae2d742429a00e68e
gventurini <giacomo.venturini3@gmail.com>
2022-07-01 23:46:00 +0200

    update quarkus and telegram bots dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
-    implementation("org.telegram:telegrambots:6.0.1")
+    implementation("org.telegram:telegrambots:6.1.0")
 
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-arc")
