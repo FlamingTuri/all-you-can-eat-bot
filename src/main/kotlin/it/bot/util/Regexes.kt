5ed767162c495aca64147a0dff7d309dc888b07c
gventurini <giacomo.venturini3@gmail.com>
2022-07-07 22:18:26 +0200

    move command matching logic to Regexes utility class

--- src/main/kotlin/it/bot/util/Regexes.kt
+++ src/main/kotlin/it/bot/util/Regexes.kt
@@ -1,6 +1,13 @@
 package it.bot.util
 
+import it.bot.model.command.BotCommand
+
 object Regexes {
 
     const val namePattern = "((\\w+)((\\s+)(\\w+))*)"
+
+    fun matchMessageWithBotCommand(botCommand: BotCommand, botUsername:String, messageText: String): MatchResult? {
+        val regex = "(?i)${botCommand.command}(@$botUsername)?(?-i)${botCommand.pattern}".toRegex()
+        return regex.matchEntire(messageText)
+    }
 }
